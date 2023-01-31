22c5f1e4d4d316a396ca0b784be385711e04b748
Tim <spridl@mail.ru>
2023-01-31 20:33:28 +0300

    Character Info ViewModel

--- RickNMortyAPI/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
+++ RickNMortyAPI/ViewModels/CharacterDetail/RMCharacterInfoCollectionViewCellViewModel.swift
@@ -5,16 +5,119 @@
 //  Created by Тимур on 26.01.2023.
 //
 
-import Foundation
+import UIKit
 
 final class RMCharacterInfoCollectionViewCellViewModel {
-    public let value: String
-    public let title: String
+    private let type: `Type`
+    private let value: String
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSZ"
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    static let shortDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    public var title: String {
+        type.displayTitle
+    }
+    public var displayValue: String {
+        if value.isEmpty { return "None" }
+        
+        if let date = Self.dateFormatter.date(from: value), type == .created {
+            return Self.shortDateFormatter.string(from: date)
+        }
+        return value
+    }
+    
+    public var iconImage: UIImage? {
+        return type.iconImage
+    }
+    
+    public var tintColor: UIColor {
+        return type.tintColor
+    }
+    
+    enum `Type`: String {
+        case status
+        case gender
+        case type
+        case species
+        case origin
+        case created
+        case location
+        case episodeCount
+        
+        var tintColor: UIColor {
+            switch self {
+            case .status:
+                return .systemRed
+            case .gender:
+                return .systemBlue
+            case .species:
+                return .systemPurple
+            case .origin:
+                return .systemGreen
+            case .created:
+                return .systemOrange
+            case .location:
+                return .systemPink
+            case .episodeCount:
+                return .systemMint
+            case .type:
+                return .systemCyan
+            }
+        }
+        
+        var iconImage: UIImage? {
+            switch self {
+            case .status:
+                return UIImage(systemName: "bell")
+            case .gender:
+                return UIImage(systemName: "bell")
+            case .species:
+                return UIImage(systemName: "bell")
+            case .origin:
+                return UIImage(systemName: "bell")
+            case .created:
+                return UIImage(systemName: "bell")
+            case .location:
+                return UIImage(systemName: "bell")
+            case .episodeCount:
+                return UIImage(systemName: "bell")
+            case .type:
+                return UIImage(systemName: "bell")
+            }
+        }
+        
+        var displayTitle: String {
+            switch self {
+            case .status,
+                    .gender,
+                    .species,
+                    .origin,
+                    .created,
+                    .location,
+                    .type:
+                return rawValue.uppercased()
+            case .episodeCount:
+                return "EPISODE COUNT"
+            }
+        }
+    }
+    
     init(
-        value: String,
-        title: String
+        type: `Type`,
+        value: String
     ) {
         self.value = value
-        self.title = title
+        self.type = type
     }
 }
--- RickNMortyAPI/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
+++ RickNMortyAPI/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
+final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterInfoCollectionViewCell"
+    
+    private let valueLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .light)
+        return label
+    }()
+    private let titleLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    private let iconImageView: UIImageView = {
+       let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.contentMode = .scaleAspectFit
+        return icon
+    }()
+    private let titleContainerView: UIView = {
+       let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addSubviews(titleContainerView ,titleLabel, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
+        setupConstraints()
         
     }
     
@@ -20,14 +52,44 @@ class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleContainerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: titleContainerView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: titleContainerView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            
+            iconImageView.heightAnchor.constraint(equalToConstant: 50),
+            iconImageView.widthAnchor.constraint(equalToConstant: 50),
+            iconImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            iconImageView.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor, constant:  -10),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
+            
+            valueLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 10),
+            valueLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            valueLabel.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor)
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        valueLabel.text = nil
+        titleLabel.text = nil
+        iconImageView.image = nil
+        iconImageView.tintColor = .label
+        titleLabel.textColor = .label
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
-        
+        titleLabel.text = viewModel.title
+        valueLabel.text = viewModel.displayValue
+        iconImageView.image = viewModel.iconImage
+        iconImageView.tintColor = viewModel.tintColor
+        titleLabel.textColor = viewModel.tintColor
     }
 }
